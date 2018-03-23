29f2d920b3e5132ddfd359c71d26496666e27411
J. Gabriel Azevedo <1160929@isep.ipp.pt>
2018-03-23 09:20:05 +0000

    Implementação TextTable

---
+++ include/ui_utils.h
@@ -0,0 +1,16 @@
+#ifndef UI_UTILS_H
+#define UI_UTILS_H
+
+
+class ui_utils
+{
+    public:
+        ui_utils();
+        virtual ~ui_utils();
+
+    protected:
+
+    private:
+};
+
+#endif // UI_UTILS_H
---
+++ src/ui_utils.cpp
@@ -0,0 +1,9 @@
+#include "ui_utils.h"
+
+ui_utils::ui_utils(){
+    //ctor
+}
+
+ui_utils::~ui_utils(){
+    //dtor
+}
