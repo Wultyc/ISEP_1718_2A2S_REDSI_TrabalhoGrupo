c07b8e2de2d2ae17c4fb84ebed39a135dd753c6e
J. Gabriel Azevedo <1160929@isep.ipp.pt>
2018-04-27 00:11:51 +0100

    Atributos Classe Loja

--- REDSI_1160929_1161573/Loja.h
+++ REDSI_1160929_1161573/Loja.h
@@ -4,7 +4,14 @@
 #include <string>
 #include <list>
 
-
+#include "Pesquisa.h"
+#include "Login.h"
+#include "ListaProdutos.h"
+#include "ListaCamara.h"
+#include "ListaObjetivas.h"
+#include "ListaTripe.h"
+#include "ListaFlash.h"
+#include "ListaStock.h"
 
 using namespace std;
 
@@ -15,7 +22,18 @@ class Loja
         virtual ~Loja();
 
     private:
+		//Pesquisa
+		Pesquisa pesquisa;
 
+		//Login Ativo
+		Login login_ativo;
+
+		//Listas
+		ListaCamaras camaras;
+		ListaObjetivas objetivas;
+		ListaFlash flashs;
+		ListaTripe tripes;
+		ListaStock stocks;
 };
 
 #endif // LOJA_H
