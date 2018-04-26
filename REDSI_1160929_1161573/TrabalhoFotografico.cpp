5e2e0a8a96fd759e5c8fe8cfa5449af60825efbd
J. Gabriel Azevedo <1160929@isep.ipp.pt>
2018-04-26 22:00:47 +0100

    Atributos classe Trabalho fotografico

--- REDSI_1160929_1161573/TrabalhoFotografico.cpp
+++ REDSI_1160929_1161573/TrabalhoFotografico.cpp
@@ -9,3 +9,68 @@ TrabalhoFotografico::~TrabalhoFotografico()
 {
     //dtor
 }
+
+Camara TrabalhoFotografico::getcamara()
+{
+	return Camara();
+}
+
+list<Objetiva> TrabalhoFotografico::getobjetivas()
+{
+	return list<Objetiva>();
+}
+
+list<Flash> TrabalhoFotografico::getflashs()
+{
+	return list<Flash>();
+}
+
+list<Tripe> TrabalhoFotografico::gettripes()
+{
+	return list<Tripe>();
+}
+
+string TrabalhoFotografico::getDate()
+{
+	return string();
+}
+
+bool TrabalhoFotografico::setCamara(Camara cam)
+{
+	return false;
+}
+
+bool TrabalhoFotografico::addObjetiva(Objetiva obj)
+{
+	return false;
+}
+
+bool TrabalhoFotografico::addFlash(Flash flash)
+{
+	return false;
+}
+
+bool TrabalhoFotografico::addTripe(Tripe tripe)
+{
+	return false;
+}
+
+bool TrabalhoFotografico::setDate(int d, int m, int y)
+{
+	return false;
+}
+
+bool TrabalhoFotografico::delObjetiva(Objetiva obj)
+{
+	return false;
+}
+
+bool TrabalhoFotografico::delFlash(Flash flash)
+{
+	return false;
+}
+
+bool TrabalhoFotografico::delTripe(Tripe tripe)
+{
+	return false;
+}
--- REDSI_1160929_1161573/TrabalhoFotografico.h
+++ REDSI_1160929_1161573/TrabalhoFotografico.h
@@ -1,6 +1,14 @@
 #ifndef TRABALHOFOTOGRAFICO_H
 #define TRABALHOFOTOGRAFICO_H
 
+#include <string>
+#include <list>
+#include "Camara.h"
+#include "Objetiva.h"
+#include "Flash.h"
+#include "Tripe.h"
+
+using namespace std;
 
 class TrabalhoFotografico
 {
@@ -8,9 +16,35 @@ class TrabalhoFotografico
         TrabalhoFotografico();
         virtual ~TrabalhoFotografico();
 
-    protected:
+		//gets
+		Camara getcamara();
+		list<Objetiva> getobjetivas();
+		list<Flash> getflashs();
+		list<Tripe> gettripes();
+		string getDate();
+
+		//sets
+		bool setCamara(Camara cam);
+		bool addObjetiva(Objetiva obj);
+		bool addFlash(Flash flash);
+		bool addTripe(Tripe tripe);
+		bool setDate(int d, int m, int y);
+
+		//remove
+		bool delObjetiva(Objetiva obj);
+		bool delFlash(Flash flash);
+		bool delTripe(Tripe tripe);
+
 
     private:
+		Camara camara;
+		list<Objetiva> objetivas;
+		list<Flash> flashs;
+		list<Tripe> tripes;
+
+		int day;
+		int month;
+		int year;
 };
 
 #endif // TRABALHOFOTOGRAFICO_H
