e30c6914c4e874c390c2be989d673c6b64455e79
J. Gabriel Azevedo <1160929@isep.ipp.pt>
2018-04-27 00:25:23 +0100

    Metodos classe loja

--- REDSI_1160929_1161573/Loja.cpp
+++ REDSI_1160929_1161573/Loja.cpp
@@ -9,3 +9,66 @@ Loja::~Loja()
 {
     //dtor
 }
+
+Pesquisa Loja::getPesquisa()
+{
+	return Pesquisa();
+}
+
+Login Loja::getLogin_ativo()
+{
+	return Login();
+}
+
+ListaCamaras Loja::getCamaras()
+{
+	return ListaCamaras();
+}
+
+ListaObjetivas Loja::getObjetivas()
+{
+	return ListaObjetivas();
+}
+
+ListaFlash Loja::getFlashs()
+{
+	return ListaFlash();
+}
+
+ListaTripe Loja::getTripes()
+{
+	return ListaTripe();
+}
+
+ListaStock Loja::getStocks()
+{
+	return ListaStock();
+}
+
+void Loja::getPesquisa(Pesquisa p)
+{
+}
+
+void Loja::getLogin_ativo(Login la)
+{
+}
+
+void Loja::getCamaras(ListaCamaras lc)
+{
+}
+
+void Loja::getObjetivas(ListaObjetivas lo)
+{
+}
+
+void Loja::getFlashs(ListaFlash lf)
+{
+}
+
+void Loja::getTripes(ListaTripe lt)
+{
+}
+
+void Loja::getStocks(ListaStock ls)
+{
+}
