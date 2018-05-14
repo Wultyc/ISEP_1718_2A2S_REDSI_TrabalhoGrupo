c0cf1498b2ca7380c0771c9b366f7f339b714080
Luis Pereira <1161573@isep.ipp.pt>
2018-05-14 16:33:49 +0100

    UC 2 (inicio)

--- REDSI_1160929_1161573/Pesquisa.cpp
+++ REDSI_1160929_1161573/Pesquisa.cpp
@@ -12,3 +12,24 @@ Pesquisa::~Pesquisa()
 {
     //dtor
 }
+
+string Pesquisa::getTipo(){
+	return tipo;
+}
+
+vector<string> Pesquisa::getParam()
+{
+	return param;
+}
+
+void Pesquisa::setTipo(string t){
+	tipo = t;
+}
+
+void Pesquisa::setParam(vector<string> p){
+	param = p;
+}
+
+void Pesquisa::importData(){
+	//Codigo SQL
+}
--- REDSI_1160929_1161573/Pesquisa.h
+++ REDSI_1160929_1161573/Pesquisa.h
@@ -1,6 +1,11 @@
 #ifndef PESQUISA_H
 #define PESQUISA_H
 
+#include <string>
+#include <vector>
+#include "Produto.h"
+
+using namespace std;
 
 class Pesquisa
 {
@@ -8,9 +13,19 @@ class Pesquisa
         Pesquisa();
         virtual ~Pesquisa();
 
-    protected:
+		//get
+		string getTipo();
+		vector<string> getParam();
+
+		//set
+		void setTipo(string t);
+		void setParam(vector<string> p);
+
+		void importData();
 
     private:
+		string tipo;
+		vector<string> param;
 };
 
 #endif // PESQUISA_H
